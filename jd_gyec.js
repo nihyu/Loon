177e5d4892e11833a877d5b6b075cbb8a3a82c86
nihyu <77567766+nihyu@users.noreply.github.com>
2021-01-18 01:31:10 +0800

    Update jd_gyec.js

--- jd_gyec.js
+++ jd_gyec.js
@@ -31,7 +31,7 @@ const $ = new Env('工业品爱消除');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let inviteCodes = [
-  '840266@2583822@2585219@2586018@1556311@2583822@2585256@2586023@2728968',
+  '840266@2583822@2585219@1012427@2586018@1556311@2583822@2585256@2586023@2728968',
   '840266@2583822@2585219@2586018@1556311@2583822@2585256@2586023@2728968',
 ]
 let exchangeName = $.isNode() ? (process.env.EXCHANGE_GYEC ? process.env.EXCHANGE_GYEC : '1888京豆') : ($.getdata('JDGYEC') ? $.getdata('JDGYEC') : '1888京豆')
